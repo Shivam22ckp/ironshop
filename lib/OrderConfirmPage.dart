e175cfab2ba7ad118b2cd50c04e656b3c4e083da
Shivam <shivam22ckp@gmail.com>
2021-02-15 23:56:53 +0530

    first commit

--- lib/OrderConfirmPage.dart
+++ lib/OrderConfirmPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ironshop/StyleScheme.dart';
 import 'package:ironshop/TrackOrderPage.dart';
+
 class OrderConfirmPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -10,6 +11,7 @@ class OrderConfirmPage extends StatelessWidget {
     );
   }
 }
+
 class orderConfirmPage extends StatefulWidget {
   @override
   _orderConfirmPageState createState() => _orderConfirmPageState();
@@ -25,11 +27,13 @@ class _orderConfirmPageState extends State<orderConfirmPage> {
         leading: IconButton(
           icon: Icon(Icons.arrow_back_ios),
         ),
-        title: Text("Order Confirmed", style: TextStyle(
-            color: Colors.black
-        ),),
+        title: Text(
+          "Order Confirmed",
+          style: TextStyle(color: Colors.black),
+        ),
         actions: [
           IconButton(
+            onPressed: () {},
             icon: Icon(Icons.search),
           )
         ],
@@ -43,28 +47,40 @@ class _orderConfirmPageState extends State<orderConfirmPage> {
               width: MediaQuery.of(context).size.width,
               height: 200,
               decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('asset/images/onBoard2.png')
-                )
-              ),
+                  image: DecorationImage(
+                      image: AssetImage('asset/images/onBoard2.png'))),
             ),
-            SizedBox(height: 15,),
-            Text("Thank you for choosing us!", style: headingStyle,),
-            Text("Your oder has been placed and we will pick up your clothes on time!", style: contentStyle,textAlign: TextAlign.center,),
-            SizedBox(height: 10,),
+            SizedBox(
+              height: 15,
+            ),
+            Text(
+              "Thank you for choosing us!",
+              style: headingStyle,
+            ),
+            Text(
+              "Your oder has been placed and we will pick up your clothes on time!",
+              style: contentStyle,
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 10,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text("Order ID", style: headingStyle,),
+                Text(
+                  "Order ID",
+                  style: headingStyle,
+                ),
                 Container(
                   padding: EdgeInsets.all(10),
                   decoration: BoxDecoration(
-                    gradient: gradientStyle,
-                    borderRadius: BorderRadius.all(Radius.circular(10))
+                      gradient: gradientStyle,
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  child: Text(
+                    "1001",
+                    style: headingStyle.copyWith(color: Colors.white),
                   ),
-                  child: Text("1001", style: headingStyle.copyWith(
-                    color: Colors.white
-                  ),),
                 )
               ],
             ),
@@ -72,60 +88,77 @@ class _orderConfirmPageState extends State<orderConfirmPage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text("Clothes Count", style: headingStyle,),
-                Text("7", style: headingStyle.copyWith(
-                  color: Colors.grey
-                ),),
+                Text(
+                  "Clothes Count",
+                  style: headingStyle,
+                ),
+                Text(
+                  "7",
+                  style: headingStyle.copyWith(color: Colors.grey),
+                ),
               ],
             ),
             divider(),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text("Final Amount", style: headingStyle,),
-                Text("225", style: headingStyle.copyWith(
-                    color: Colors.grey
-                ),),
+                Text(
+                  "Final Amount",
+                  style: headingStyle,
+                ),
+                Text(
+                  "225",
+                  style: headingStyle.copyWith(color: Colors.grey),
+                ),
               ],
             ),
             divider(),
             Column(
-
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text("Pick up Date & Time", style: headingStyle,),
-                Text("Wednesday, 07 Aug, 2020. Between 10:00 AM & 12:00 PM", style: contentStyle.copyWith(
-                    color: Colors.grey,
-                  fontSize: 16
-                ),),
+                Text(
+                  "Pick up Date & Time",
+                  style: headingStyle,
+                ),
+                Text(
+                  "Wednesday, 07 Aug, 2020. Between 10:00 AM & 12:00 PM",
+                  style:
+                      contentStyle.copyWith(color: Colors.grey, fontSize: 16),
+                ),
               ],
             ),
             divider(),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text("Payment Method", style: headingStyle,),
-                Text("Cash", style: headingStyle.copyWith(
-                    color: Colors.grey
-                ),),
+                Text(
+                  "Payment Method",
+                  style: headingStyle,
+                ),
+                Text(
+                  "Cash",
+                  style: headingStyle.copyWith(color: Colors.grey),
+                ),
               ],
             ),
             Expanded(
               child: Container(),
             ),
             InkWell(
-              onTap: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>TrackOrderPage()));},
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => TrackOrderPage()));
+              },
               child: Container(
                 width: MediaQuery.of(context).size.width,
                 padding: EdgeInsets.symmetric(vertical: 20),
-                decoration: BoxDecoration(
-                    gradient: gradientStyle
-                ),
+                decoration: BoxDecoration(gradient: gradientStyle),
                 child: Center(
-                  child: Text("TRACK ORDER", style: contentStyle.copyWith(
-                      color: Colors.white,
-                      fontSize: 22
-                  ),),
+                  child: Text(
+                    "TRACK ORDER",
+                    style: contentStyle.copyWith(
+                        color: Colors.white, fontSize: 22),
+                  ),
                 ),
               ),
             )
@@ -134,8 +167,8 @@ class _orderConfirmPageState extends State<orderConfirmPage> {
       ),
     );
   }
-  Container divider()
-  {
+
+  Container divider() {
     return Container(
       margin: EdgeInsets.symmetric(vertical: 15),
       height: 1,
@@ -143,4 +176,3 @@ class _orderConfirmPageState extends State<orderConfirmPage> {
     );
   }
 }
-
